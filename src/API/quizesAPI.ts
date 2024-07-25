2cf10b0aa1f35a9efb14f6c91c90c32ffd48a67e
Salama <elmohamedahmedsalama@gmail.com>
2024-07-26 02:32:00 +0300

    change in api axios

--- src/API/quizesAPI.ts
+++ src/API/quizesAPI.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-const api="https://anyware-backend-knn4r5ktd-mohamed-salamas-projects-ee33a6a6.vercel.app"
+const api="https://anyware-backend-b31xk8mgr-mohamed-salamas-projects-ee33a6a6.vercel.app"
 
 export const listQuizesAPI=async()=>{
     const response=await axios.get(`${api}/listQuizes`);
