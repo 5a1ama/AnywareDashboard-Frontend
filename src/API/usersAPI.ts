2bbb86d77cd049d8d214ebd8fc7ef10a4ea00059
Salama <elmohamedahmedsalama@gmail.com>
2024-07-26 02:28:58 +0300

    change in api axios

--- src/API/usersAPI.ts
+++ src/API/usersAPI.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-const api="https://anyware-backend-9z3qid64k-mohamed-salamas-projects-ee33a6a6.vercel.app"
+const api="https://anyware-backend-knn4r5ktd-mohamed-salamas-projects-ee33a6a6.vercel.app"
 
 export const loginAPI=async (email: string, password: string)=>{
   try {
