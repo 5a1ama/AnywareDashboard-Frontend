e44685a432c9bfbfaf76f2be1d2df9a81569f564
Salama <elmohamedahmedsalama@gmail.com>
2024-07-25 22:59:38 +0300

    updated deploy API for axios

--- src/API/announcementsAPI.ts
+++ src/API/announcementsAPI.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-const api="https://anyware-backend-2pms1l57b-mohamed-salamas-projects-ee33a6a6.vercel.app/"
+const api="https://anyware-backend-n41jp0j78-mohamed-salamas-projects-ee33a6a6.vercel.app/"
 
 export const listAnnouncementsAPI=async()=>{
     const response=await axios.get(`${api}/listAnnouncements`);
